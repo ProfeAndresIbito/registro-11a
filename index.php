c52484116a2ae65fc5cb4c92ffba8e37947acb89
ProfeAndresIbito <andresibito@comfandi.edu.co>
2025-04-02 13:55:10 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <h2>Hola</h2>
+        <p>Inicia tu Registro</p>
+
+        <div class="input-wrapper">
+            <input type="text" name="name" placeholder="Nombre">
+            <img class="input-icon" src="images/name.svg" alt="">
+        </div>
+
+        <div class="input-wrapper">
+            <input type="email" name="email" placeholder="Email">
+            <img class="input-icon" src="images/email.svg" alt="">
+        </div>
+
+        <div class="input-wrapper">
+            <input type="text" name="direction" placeholder="Dirección">
+            <img class="input-icon" src="images/direction.svg" alt="">
+        </div>
+
+        <div class="input-wrapper">
+            <input type="tel" name="phone" placeholder="Teléfono">
+            <img class="input-icon" src="images/phone.svg" alt="">
+        </div>
+
+        <div class="input-wrapper">
+            <input type="password" name="password" placeholder="Contraseña">
+            <img class="input-icon" src="images/password.svg" alt="">
+        </div>
+
+        <input class="btnEnviar" type="submit" name="register" value="Enviar"> 
+    </form>
+</body>
+</html>
